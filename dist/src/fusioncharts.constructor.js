9ae1549b71a9776bbc65e07b1add275f255ffc77
suresh kumar <suresh.kumar@accolitedigital.com>
2022-05-29 02:15:59 +0530

    Removed folder from repository

--- dist/src/fusioncharts.constructor.js
+++
@@ -1,5 +0,0 @@
-export function FusionChartsConstructor(fusionchartsService, chartConfig) {
-    var FusionCharts = fusionchartsService.getFusionChartsStatic();
-    return new FusionCharts(chartConfig);
-}
-//# sourceMappingURL=fusioncharts.constructor.js.map
--- dist/src/fusioncharts.pipe.d.ts
+++
@@ -1,7 +0,0 @@
-import { PipeTransform } from '@angular/core';
-/**
- * Transforms any input value
- */
-export declare class FusionChartsPipe implements PipeTransform {
-    transform(value: any, args?: any[]): string;
-}
